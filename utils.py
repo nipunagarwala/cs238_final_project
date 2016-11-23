9680af8ebd006ead95dbae280df6ca123f463023
root <root@group12.54y3r0tb5ayedmdhuhnixbldqh.jx.internal.cloudapp.net>
2016-11-23 23:46:21 +0000

    adding utils.py and extracted preprocessing features of Rochester code

--- utils.py
+++ utils.py
@@ -2,20 +2,26 @@ import sgf
 import os
 import h5py
 import numpy as np
+from Rocgame_converter import *
 
+# constants
 # map from numerical coordinates to letters used by SGF
 SGF_POS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 BOARD_SZ = 9
+# default feature list for the state generation
+FEATURE_LIST = ["board", "ones", "turns_since", "liberties", "capture_size",
+                "self_atari_size", "liberties_after", "ladder_capture", 
+                "ladder_escape", "sensibleness", "zeros"]
 
 def sgfWriter(actions, filename, boardSz=9):
     """
     Creates an SGF file from the input 'actions'
 
-    @type   actions     :   (string, int)
-    @param  patientID   :   Records the actions taken in a game
-                            String should be 'B' for black or 'W' for white
-                            Int should be the location of the action, zero-indexed
+    @type   actions     :   Integer List
+    @param  actions     :   Records the actions taken in a game
+                            Each integer should be the location of the action, zero-indexed
                             from the top left, row-major order.
+                            Black goes first, as specified via go standard rule.
     @type   filename    :   String
     @param  filename    :   Name of the file to save the sgf file to.
     @type   boardSz     :   int
@@ -35,16 +41,20 @@ def sgfWriter(actions, filename, boardSz=9):
     initNode.first = True
 
     prevNode = initNode
-    for playColor,hand in actions:
+    playColors = ['B','W']
+    count = 0
+    for act in actions:
         currNode = sgf.Node(gtree, previous=None)
-        sgfHand = SGF_POS[hand%BOARD_SZ]+SGF_POS[hand/BOARD_SZ]
-        currNode.properties = {playColor: [sgfHand]}
+        sgfAct = SGF_POS[act%BOARD_SZ]+SGF_POS[act/BOARD_SZ]
+        currNode.properties = {playColors[count]: [sgfAct]}
 
         prevNode.next = currNode
         currNode.previous = prevNode
         
         gtree.nodes.append(prevNode)
         prevNode = currNode
+
+        count = (count+1)%2
 
     gtree.nodes.append(currNode)
 
@@ -64,18 +74,14 @@ def sgf2hdf5(filename, sgfDir, boardSz=BOARD_SZ):
     @type   boardSz     :   int
     @param  boardSz     :   Size of the board. Defaults to 9.
     """
-    preprocess_cmd_temp = 'python -m game_converter --outfile %s --directory %s --size %d --features all'
-    preprocess_cmd = preprocess_cmd_temp %(filename,sgfDir,boardSz)
-    os.system(preprocess_cmd)
+    run_game_converter(['--outfile', filename, '--directory', sgfDir, '--size', str(boardSz), '--features', 'all'])
 
-def sgf2stateaction(sgfFilename, boardSz=BOARD_SZ):
+def hdf52stateaction(hdf5Filename):
     """
-    Converts an SGF file of a game and returns a tuple of state and action pairs
+    Loads an HDF5 file of a game and returns a tuple of state and action pairs
 
-    @type   sgfFilename :   String
-    @param  sgfFilename :   Filename of the sgf file.
-    @type   boardSz     :   int
-    @param  boardSz     :   Size of the board. Defaults to 9.
+    @type   hdf5Filename:   String
+    @param  hdf5Filename:   Filename of the hdf5 file.
 
     @rtype              :   A tuple of numpy matrices
     @return             :   np_states stores the state information at each time slot
@@ -85,14 +91,74 @@ def sgf2stateaction(sgfFilename, boardSz=BOARD_SZ):
                                 np_acts - (npositions)*2, where 2 corresponds to
                                           x and y coordinates
     """
-    filename = 'tmp.hdf5'
-    preprocess_cmd_temp = 'find "%s" | python -m game_converter --outfile %s --size %d --features all'
-    preprocess_cmd = preprocess_cmd_temp %(sgfFilename, filename, boardSz)
-    os.system(preprocess_cmd)
-
-    with h5py.File(filename,'r') as hf:
+    with h5py.File(hdf5Filename,'r') as hf:
         np_states = np.array(hf.get("states"))
         np_acts = np.array(hf.get("actions"))
 
-    os.remove(filename)
     return (np_states, np_acts)
+
+def sgf2stateaction(filename, boardIndx, feature_list=FEATURE_LIST):
+    """
+    Loads an SGF file and returns a tuple of state and action pairs
+
+    @type   filename    :   String
+    @param  filename    :   Name of the SGF file.
+    @type   boardIndx   :   Integer List
+    @param  boardIndx   :   List of board locations to extract the state-action pairs from.
+                            0 indexed, time slice 0 refers to the blank board. 
+                            In other words, the first action (Black) occurs at index 0.
+    @type   feature_list:   String List
+    @param  feature_list:   State features. Refer to FEATURE_LIST for how to specify them.
+
+    @rtype              :   A tuple of numpy matrices
+    @return             :   states stores the state information at each time slot
+                            actions stores the action information at each time slot
+                            Empty tuple returned when error
+                            Dimensionalities:
+                                states - (npositions)*48*boardSz*boardSz
+                                actions - (npositions)*2, where 2 corresponds to
+                                          x and y coordinates
+    """
+    converter = game_converter(feature_list)
+
+    states = np.empty([len(boardIndx),48,BOARD_SZ,BOARD_SZ])
+    actions = np.empty([len(boardIndx),1])
+
+    indx = 0
+    count = -1
+    try:
+        for state,action in converter.convert_game(filename, bd_size=BOARD_SZ):
+            count += 1
+            if count not in boardIndx:
+                continue
+            states[indx,:,:,:] = state
+            actions[indx,:] = action[0]+action[1]*BOARD_SZ
+            indx += 1
+
+        if count<max(boardIndx):
+            warnings.warn("%d is larger than how many hands were played in %s; " % (max(boardIndx),filename))
+            return ()
+
+    except go.IllegalMove:
+        warnings.warn("Illegal Move encountered in %s\n"
+                      "\tdropping the remainder of the game" % filename)
+        return ()
+    except sgf.ParseException:
+        warnings.warn("Could not parse %s\n\tdropping game" % filename)
+        return ()
+    except SizeMismatchError:
+        warnings.warn("Skipping %s; wrong board size" % filename)
+        return ()
+    except Exception as e:
+        # catch everything else
+        warnings.warn("Unkown exception with file %s\n\t%s" % (filename, e),
+                      stacklevel=2)
+        return ()
+
+    return (states,actions)
+
+# Usage:
+# sgfWriter([0,1,2,3,4,9,10],'example.sgf')
+# s,a = sgf2stateaction('example.sgf',[0,1,2,3,4,5,6])
+# print s[6,0:3,:,:]
+# print a
