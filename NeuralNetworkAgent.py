7ec1539a80a6b027801bea32008b41cb20b7b4eb
Nipun Agarwala <nipuna1@group12.54y3r0tb5ayedmdhuhnixbldqh.jx.internal.cloudapp.net>
2016-12-01 04:44:43 +0000

    Added Neural NEtwork agent.

--- NeuralNetworkAgent.py
+++ NeuralNetworkAgent.py
@@ -3,6 +3,25 @@ import numpy as np
 from utils import *
 from goUtils import *
 from layers import *
+from hdf5Reader import *
 
 
-class NeuralNetworkAgent(Object):
+# class NeuralNetworkAgent(Object):
+# 	def __init__(self):
+# 		self.hdf5Rd = HDF5Reader()
+# 		self.hdf5Rd.readData()
+
+
+# 	def trainSupervisedPolicyNetwork(self):
+
+
+
+
+# 	# Take current state as input, and then return an action prediction
+# 	def make_move(self, state):
+
+
+
+	
+
+
--- hdf5Reader.py
+++ hdf5Reader.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
 import numpy as np
 import h5py
- 
+
+
+# class HDF5Reader(object):
+# 	def __init__(self, fileName):
+# 		self.fileName = fileName
+
+# 	def getData(self):
 with h5py.File('features.hdf5','r') as hf:
     datasets = hf.keys()
     print('List of arrays in this file:\n\t%s\n' %datasets)
